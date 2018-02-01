96fbcdc043db71cd2d5789077382a00b0ee48905
Ben <ben.bariteau@gmail.com>
2018-01-31 22:47:38 -0800

    parse episode file and serve it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-extern crate iron;
+extern crate serde_json;
 
+#[macro_use] extern crate iron;
 #[macro_use] extern crate askama;
+#[macro_use] extern crate serde_derive;
 
+use std::fs::File;
 use iron::middleware::Chain;
 use iron::Iron;
 use iron::status;
@@ -10,9 +13,22 @@ use iron::IronResult;
 use iron::request::Request;
 use askama::Template;
 
+#[derive(Serialize, Deserialize)]
+struct Episode {
+    Season: i8,
+    Title: String,
+    Link: String,
+    EpisodeNum: String,
+    Description: String,
+    Series: String,
+}
+
 
 fn app(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello world")))
+    let file = itry!(File::open("sorted_eps.json"));
+    let episodes: Vec<Episode> = itry!(serde_json::from_reader(file));
+    let episodes_json = itry!(serde_json::to_string_pretty(&episodes));
+    Ok(Response::with((status::Ok, episodes_json)))
 }
 
 fn main() {
