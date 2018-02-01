682d10cdc5db93a9457ce7649fccbaecae5c331b
Ben <ben.bariteau@gmail.com>
2018-01-31 22:03:09 -0800

    add askama as a dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate iron;
+
+#[macro_use] extern crate askama;
+
+use iron::middleware::Chain;
+use iron::Iron;
+use iron::status;
+use iron::response::Response;
+use iron::IronResult;
+use iron::request::Request;
+use askama::Template;
+
+
+fn app(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello world")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let chain = Chain::new(app);
+    Iron::new(chain).http("0.0.0.0:3000").unwrap();
 }
