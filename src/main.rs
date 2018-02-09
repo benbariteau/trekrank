842154e1698be0640bfb8f5fcd8c9776f58df6c5
Ben <ben.bariteau@gmail.com>
2018-02-08 22:06:16 -0800

    parse description param

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate serde_json;
+extern crate params;
 
 #[macro_use] extern crate askama;
 #[macro_use] extern crate iron;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate error_chain;
 
 use askama::Template;
 use iron::Iron;
@@ -13,6 +15,9 @@ use iron::request::Request;
 use iron::response::Response;
 use iron::status;
 use std::fs::File;
+use params::Params;
+use params::Value;
+use iron::Plugin;
 
 #[derive(Serialize, Deserialize)]
 struct Episode {
@@ -35,11 +40,32 @@ struct App {
     episodes: Vec<RankedEpisode>,
 }
 
+mod error {
+    error_chain!{}
+}
 
-fn app(_: &mut Request) -> IronResult<Response> {
+
+fn app(req: &mut Request) -> IronResult<Response> {
+    let params = req.get::<Params>().unwrap();
+
+    let show_description = itry!(params.find(&["description"]).map_or_else(
+        || Ok(false),
+        |ref value| -> Result<bool, error::Error> {
+            match value {
+                &&Value::String(ref string) => {
+                    if string == "show" {
+                        Ok(true)
+                    } else {
+                        Err("invalid value for description".into())
+                    }
+                }
+                _ => Err("invalid type for description".into())
+            }
+        },
+    ));
+
     let file = itry!(File::open("star_trek_rank.json"));
     let episodes: Vec<Episode> = itry!(serde_json::from_reader(file));
-
     let ranked_episodes: Vec<RankedEpisode> = episodes.into_iter().enumerate().map(|(rank, episode)| RankedEpisode{rank: rank as u16, episode: episode}).collect();
 
     let mut response = Response::with((
