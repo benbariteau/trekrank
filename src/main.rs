c690134c6b7368d0931d768a536ef75fb91deafe
Ben <ben.bariteau@gmail.com>
2018-02-08 21:16:18 -0800

    create RankedEpisode type for filtering

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,15 @@ struct Episode {
     series: String,
 }
 
+struct RankedEpisode {
+    rank: u16,
+    episode: Episode,
+}
+
 #[derive(Template)]
 #[template(path="app.tmpl.html")]
 struct App {
-    episodes: Vec<Episode>,
+    episodes: Vec<RankedEpisode>,
 }
 
 
@@ -35,9 +40,11 @@ fn app(_: &mut Request) -> IronResult<Response> {
     let file = itry!(File::open("star_trek_rank.json"));
     let episodes: Vec<Episode> = itry!(serde_json::from_reader(file));
 
+    let ranked_episodes: Vec<RankedEpisode> = episodes.into_iter().enumerate().map(|(rank, episode)| RankedEpisode{rank: rank as u16, episode: episode}).collect();
+
     let mut response = Response::with((
         status::Ok,
-        itry!(App{episodes: episodes}.render())
+        itry!(App{episodes: ranked_episodes}.render())
     ));
     response.headers.set(ContentType::html());
     Ok(response)
